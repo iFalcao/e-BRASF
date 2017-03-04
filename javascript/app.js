0d5a0b5d765c931c714177e122141c72eecd3c74
Icaro <vbsfalcao@gmail.com>
2017-03-04 00:24:01 -0300

    Add Google Maps API + change page footer

--- javascript/app.js
+++ javascript/app.js
@@ -4,3 +4,17 @@ alternador.onclick = function() {
 	var menu = document.querySelector('.js-menu');
 	menu.classList.toggle('menu--exibindo');
 }
+
+function initMap() {
+	var salvador = {lat: -12.977145, lng: -38.446708};
+	var map = new google.maps.Map(document.getElementById('map'), {
+	    zoom: 15,
+	    center: salvador
+	});
+
+	var marker = new google.maps.Marker({
+		position: salvador,
+		map: map
+	});
+}
+		
